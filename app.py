bf35366e7335f4d506bec72cd3bb2bf1783a4b07
zshadow001 <parmarprince1982010@gmail.com>
2026-05-29 12:22:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ client.connect()
 
 me = client.get_me()
 
+
 @app.route("/")
 def home():
     return render_template(
@@ -27,6 +28,43 @@ def home():
         username=me.username,
         phone=me.phone
     )
+
+
+@app.route("/profile")
+def profile():
+    me = client.get_me()
+
+    return render_template(
+        "profile.html",
+        me=me
+    )
+
+
+@app.route("/chats")
+def chats():
+    dialogs = client.get_dialogs(limit=100)
+
+    return render_template(
+        "chats.html",
+        dialogs=dialogs
+    )
+
+
+@app.route("/channels")
+def channels():
+    dialogs = client.get_dialogs(limit=100)
+
+    channel_list = []
+
+    for d in dialogs:
+        if d.is_channel:
+            channel_list.append(d)
+
+    return render_template(
+        "channels.html",
+        channels=channel_list
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
